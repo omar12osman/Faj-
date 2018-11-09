f11cb0a5218e975d4f545ed7d6f3dbbadcbaa983
Your Actual Namemajid <Your Actual Emailmajid1osman@gmail.com>
2018-11-09 15:22:19 +0100

    Make some changes  to the framework

---
+++ Faj/All Controllers are here/AddThingsUserPageController.swift
@@ -0,0 +1,20 @@
+//
+//  AddThingsUserPageController.swift
+//  Faj
+//
+//  Created by Majid Osman on 2018-11-08.
+//  Copyright © 2018 Majid Osman. All rights reserved.
+//
+
+import UIKit
+class AddThingsUserPageController : UITableViewController {
+    //
+    //    let AddtableView : UITableView = {
+    //    let addtableView = UITableView()
+    //    }
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = .white
+    }
+}
---
+++ Pods/Target Support Files/Pods-Faj/Pods-Faj-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Faj : NSObject
+@end
+@implementation PodsDummy_Pods_Faj
+@end
